1c67e6b3aab36ab5ccae0ca4e4b423b7949d994a
kindtiger <seonbeom2@gmail.com>
2022-12-30 09:37:03 +0900

    [수정] userName 코드 수정

--- client/chapter/core/08-2.condition.js
+++ client/chapter/core/08-2.condition.js
@@ -28,24 +28,28 @@ let whichFalsy = true && ' ' && [] && { thisIsFalsy: false };
 // 첫번째 Truthy를 찾는 연산 (||)
 let whichTruthy = false || '' || [2, 3].length || { thisIsTruthy: true };
 
+
+
 let userName = prompt('사용자 아이디를 입력해주세요.','');
 
+// console.log(userName);
+if(userName?.toLowerCase() === 'admin'){
 
-
-if(userName === 'Admin'){
   let pw = prompt('비밀번호를 입력해 주세요.','');
 
-  if(pw === 'TheMaster'){
+  if(pw?.toLowerCase() === 'themaster'){
     console.log('환영합니다.');
   }else{
     console.log('취소되었습니다.');
   }
-
-}else if(userName === '' || userName === null){
+}else if(userName.replace(/\s*/g,'') === '' || userName === null){
   console.log('취소했습니다.');
 }else{
   console.log('인증되지 않은 사용자 입니다.');
 }
+
+
+
 
 // console.log(userName);
 
