88d50b72f26ef30c44af7145430640f3c3b12204
rodwnl <qurtks2224@gmail.com>
2023-01-06 15:53:52 +0900

    DOM click event

---
+++ client/lib/dom/getNode.js
@@ -0,0 +1,60 @@
+/* ---------------------------------------------------------------------- */
+/* DOM traversal                                                          */
+/* ---------------------------------------------------------------------- */
+
+
+/* 모든 노드에서 사용 */
+// - parentNode
+// - childNodes
+// - firstChild
+// - lastChild
+// - previousSibling
+// - nextSibling
+
+/* 요소 노드에서만 사용 가능 */
+// - parentElement
+// - children
+// - firstElementChild
+// - lastElementChild
+// - previousElementSibling
+// - nextElementSibling
+
+/* 문서 대상 찾기 */
+// - getElementById
+// - getElementsByTagName
+// - getElementsByClassName
+// - querySelector
+// - querySelectorAll
+// - closest
+
+
+// let first = document.querySelector('.first');
+
+let [first,second] = document.querySelectorAll('span');
+
+
+// console.log(second);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 문서 대상 확인 */
+// - matches
+// - contains
+
+
+
+
+
+
