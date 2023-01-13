359cc50bfc16cedc151020c44b70f2f617b48d5d
rodwnl <qurtks2224@gmail.com>
2023-01-13 16:05:54 +0900

    jujeob

---
+++ client/lib/utils/typeOf.js
@@ -0,0 +1,4 @@
+export function isNumbericString(data){
+  data=Number(data);
+  return !isNaN(data)
+}
--- client/main.js
+++ client/main.js
@@ -1,5 +1,32 @@
-import { getNode } from './lib/index.js'
+import { addClass, clearContents, getInputValue, getNode, getRandom, insertLast, isNumbericString, removeClass, showAlert } from './lib/index.js'
+import { jujeobData } from "./data/data.js";
 
 const submit = getNode('#submit');
+const result = getNode('.result');
 
 console.log(submit);
+
+function clickSubmitHandler(e){
+  e.preventDefault();
+
+  let name = getInputValue('#nameField');
+  let list = jujeobData(name)
+  let pick = list[getRandom(list.length-1)];
+
+  console.log(pick);
+
+  if (!name) {
+    showAlert(".alert", "이름을 입력해주세요", 3000);
+    return;
+  }
+
+  if (isNumericString(name)) {
+    showAlert(".alert", "제대로된 이름을 입력해주세요", 3000);
+    return;
+  }
+
+  clearContents(result);
+  insertLast(result,pick);
+}
+
+submit.addEventListener('click', clickSubmitHandler)
