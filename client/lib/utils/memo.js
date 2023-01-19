b3e57e01f16a2dfe9888c4cdf0bfffdfb10a9c86
rodwnl <qurtks2224@gmail.com>
2023-01-19 15:03:35 +0900

    json 서버 연동

--- client/lib/utils/memo.js
+++ client/lib/utils/memo.js
@@ -23,24 +23,23 @@ export const memo = (() => {
 })()
 
 
-
-// memo('name',()=>'tiger')
-// memo('name')
-
-// console.log(memo('name',()=>'tttt'));
+// rendingUserList 함수 만들기 
+// ajax (tiger) get user List
 
 
+// 유저 카드 생성
+// 생성된 카드로 랜더링 
+
+async function rendingUserList(){
+
+  let response = await tiger.get('https://jsonplaceholder.typicode.com/users/1')
+
+  let userData = response.data;
+
+  console.log(userData);
 
 
-// memo()('cube',()=> document.querySelector('#cube'));
-
-
-
-// console.log( memo()('cube') );
-
-
-
-
+}
 
 
 
--- client/main.js
+++ client/main.js
@@ -1,4 +1,6 @@
-import { xhrData, insertLast, xhrPromise, tiger, delayP } from "./lib/index.js";
+/* global gsap */ 
+
+import { getNode, xhrData, insertLast, xhrPromise, tiger, delayP, renderUserCard, changeColor, renderSpinner, attr, renderEmptyCard } from "./lib/index.js";
 
 
 /*
@@ -26,14 +28,69 @@ xhrPromise
 })
 */
 
+// 유저 카드 생성
+// 생성된 카드로 렌더링
 
-async function render(){
+// 1. userList.js 로 간다
+// 2. renderUserCard 함수를 만들기
+// 3. 만들어진 함수 안에 createUserCard 를 던지고,
+// 4. renderUserCard 함수를 사용했을 때 렌더링이 잘 될 수 있도록
 
-  await delayP(2000);
-  let response = await tiger.get('https://jsonplaceholder.typicode.com/users/1')
+const userCardContainer = getNode('.user-card-inner');
 
-  console.log(response.data);
+async function rendingUserList() {
+
+  renderSpinner(userCardContainer)
+
+  try{
+    await delayP(2000)
+    getNode('.loadingSpinner').remove();
+
+    let response = await tiger.get( 'http://localhost:3000/users' );
+
+    let userData = response.data;
+    // userData.forEach(data=> renderUserCard(userCardContainer,data))
+    //배열 순환에는 forEach...
+    //리턴이 필요한 건 map, reduce, 리턴이 필요없는 건 forEach
+    userData.forEach((data)=> {
+      renderUserCard(userCardContainer,data)
+    })
+
+    changeColor('.user-card');
+
+    gsap.to(gsap.utils.toArray('.user-card'),{
+      x:0,
+      opacity:1,
+      duration:1.5,
+      stagger: 0.2,
+    })
+  }catch(err){
+    console.log(err);
+    renderEmptyCard(userCardContainer);
+  }
 }
 
+rendingUserList();
 
-render()
+// 삭제 버튼을 클릭하면 콘솔창에 '삭제' 글자가 출력이 될 수 있도록 만들어 주세요.
+
+function handler(e){
+  let deleteButton = e.target.closest('button');
+  let article = e.target.closest('article');
+
+  // 버튼이 아니면 실행 안함
+  // 누른 대상의 인접한 대상이 article이 아니면 실행 안함
+  if(!deleteButton|| !article) return;
+  console.log(deleteButton);
+
+  let id=attr(article, 'data-index').slice(5); //index-1 로 표시되서 숫자만 나오겠끔 잘랐음
+
+  tiger.delete(`http://localhost:3000/users/${id}`).then(()=>{
+    userCardContainer.innerHTML= '';
+    rendingUserList();
+  })
+
+
+}
+
+userCardContainer.addEventListener('click', handler);
